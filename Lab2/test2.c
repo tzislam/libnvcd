1537eeb29a3569087d3885f28df75d17232999d4
schutth <schutth@wwu.edu>
2019-04-15 10:06:44 -0700

    initial files for lab2

---
+++ Lab2/test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+float serial_dot_prod(float* a, float* b, int N) 
+{
+  float sum = 0.0;
+  int i;
+  for(int i=0; i<N; i++) {
+     sum += a[i] * b[i];
+   }
+  return sum;
+}
+
+
+int main(int argc, char** argv) {
+	int N = 100;
+	// Create an array of floats a and fill it with N random numbers
+	// Create an array of floats b and fill it with N random numbers
+	// Call serial_dot_prod(a, b, N) and store results in a temporary array
+	// Implement a parallel_dot_prod(a, b, N) using OpenMP
+	// Call parallel_dot_prod(a, b, N) and store results in another temporary array
+	// Compare the two results. If the resutls match, output "Correct" or elsee output "Incorrect"
+
+}
