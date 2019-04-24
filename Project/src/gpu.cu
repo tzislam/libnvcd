f8202b451c60c843c3b74b05af6a98ef767ac65c
schutth <schutth@wwu.edu>
2019-04-24 00:25:39 -0700

    z-indexing

--- Project/src/gpu.cu
+++ Project/src/gpu.cu
@@ -1,16 +1,23 @@
 #include "common.h"
 
-extern "C" {
-	GPU_FN void assert_cond_impl(bool condition, const char* message)
+#define GPU_ASSERT(condition_expr) assert_cond_impl(condition_expr, #condition_expr, __LINE__) 
+
+extern "C" {	
+	GPU_FN bool assert_cond_impl(bool condition, const char* message, int line)
 	{
 		if (!condition) {
-			printf("DEVICE ERROR: %s\n", message);
+			printf("DEVICE ASSERTION FAILURE: %s (line %i)\n", message, line);
 		}
+
+		return condition;
 	}
 	
 	GPU_FN void print_thread_info()
 	{
-		int thread = threadIdx.x + threadIdx.y * blockDim.x;
+		int thread =
+			threadIdx.x +
+			threadIdx.y * blockDim.x +
+			threadIdx.z * blockDim.x * blockDim.y;
 		
 		printf("the thread: %i\n", thread);
 	}
@@ -23,7 +30,7 @@ extern "C" {
 	GPU_CLIENT_FN void gpu_test()
 	{
 		dim3 grid(1, 1, 1);
-		dim3 block(2, 2, 1);
+		dim3 block(2, 2, 2);
 	
 		gpu_kernel<<<grid, block>>>();
 	}
