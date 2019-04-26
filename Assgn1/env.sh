a516720e558e0a68a59744fe5437cec492376b4a
schutth <schutth@wwu.edu>
2019-04-26 00:08:16 -0700

    experimenting...

--- Assgn1/env.sh
+++ Assgn1/env.sh
@@ -11,35 +11,41 @@ CORES_PER_NODE=$(echo "${CORES_PER_SOCKET} * ${SOCKETS_PER_NODE}" | bc)
 TOTAL_CORES=$(echo "${CORES_PER_NODE} * ${NUM_NODES}" | bc)
 preset=$1
 
-echo "TOTAL_CORES: " $TOTAL_CORES
-echo "CORES PER NODE: " $CORES_PER_NODE
+#echo "TOTAL_CORES: " $TOTAL_CORES
+#echo "CORES PER NODE: " $CORES_PER_NODE
+
+NUM_THREADS=12
 
 sockets_close() {
     echo "exec policy: sockets_close"
     export OMP_PLACES=sockets
     export OMP_PROC_BIND=close
-    export OMP_NUM_THREADS=$CORES_PER_SOCKET
+    #export OMP_NUM_THREADS=$CORES_PER_SOCKET
+#    export OMP_NUM_THREADS=$NUM_THREADS
 }
 
 sockets_spread() {
     echo "exec policy: sockets_spread"
     export OMP_PLACES=sockets
     export OMP_PROC_BIND=spread
-    export OMP_NUM_THREADS=$CORES_PER_NODE
+#    export OMP_NUM_THREADS=$CORES_PER_NODE
+ #   export OMP_NUM_THREADS=$NUM_THREADS
 }
 
 cores_close() {
     echo "exec policy: cores_close"
     export OMP_PLACES=cores
     export OMP_PROC_BIND=close
-    export OMP_NUM_THREADS=$THREADS_PER_CORE
+    #    export OMP_NUM_THREADS=$THREADS_PER_CORE
+  #  export OMP_NUM_THREADS=$NUM_THREADS
 }
 
 cores_spread() {
     echo "exec policy: cores_spread"
     export OMP_PLACES=cores
     export OMP_PROC_BIND=spread
-    export OMP_NUM_THREADS=$CORES_PER_SOCKET
+    #    export OMP_NUM_THREADS=$CORES_PER_SOCKET
+   # export OMP_NUM_THREADS=$NUM_THREADS
 }
 
 case "$preset" in
