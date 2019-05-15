fca422ba7e5ff1fef1612fd76543eaa2b45725e9
schutth <schutth@wwu.edu>
2019-05-14 22:43:38 -0700

    easier for sandbox testing if we let main be defined in cuda c file

---
+++ Project/lib/test.cu
@@ -0,0 +1,59 @@
+#include "gpumon.h"
+#include "gpumon.cuh"
+#include "util.h"
+#include <stdio.h>
+
+#ifdef SANDBOX
+__device__ int* const_symbol;
+
+/********************/
+/* CUDA ERROR CHECK */
+/********************/
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+/***************/
+/* TEST KERNEL */
+/***************/
+__global__ void kernel() {
+
+	printf("Address of symbol from device = %p\n", &const_symbol);
+	const_symbol[0] = 2;
+	const_symbol[1] = 3;
+	const_symbol[2] = 3;
+}
+#endif
+
+int main() {
+#ifdef SANDBOX
+	const int N = 16;
+
+	void* d_const_sym = __cuda_zalloc_sym(sizeof(int) * 3, const_symbol);
+	
+	kernel<<<1,1>>>();
+
+	CUDA_RUNTIME_FN(cudaDeviceSynchronize());
+	
+	int k[3];
+
+	CUDA_RUNTIME_FN(cudaMemcpy(k, d_const_sym, sizeof(int) * 3, cudaMemcpyDeviceToHost));
+
+	for(int i = 0; i < 3; ++i) {
+		printf("k[%i] = %i\n", i, k[i]);
+	}
+#else
+	gpumon_host_start(1024);
+
+	gpumon_kernel_test_call(1024);
+	
+	gpumon_host_end();
+#endif
+	return 0;
+}
