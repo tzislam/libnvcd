c8b0e965de277ddd9d4c3ddaff903409c0fecba4
schutth <schutth@wwu.edu>
2019-05-14 17:20:49 -0700

    renamed these

--- Project/lib/gpumon.cu
+++
@@ -1,9 +0,0 @@
-#include "gpumon.h"
-
-#include <cuda.h>
-
-static __device__ clock64_t* dev_tstart = nullptr;
-static __device__ clock64_t* dev_ttime = nullptr;
-
-
-
--- Project/lib/test.cu
+++
@@ -1,9 +0,0 @@
-#include "gpumon.h"
-
-int main() {
-	gpumon_host_start(1024);
-
-	gpumon_host_end();
-
-	return 0;
-}
