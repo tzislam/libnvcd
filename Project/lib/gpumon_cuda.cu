7bbca40331c89a7084dde093141f638336e93a27
schutth <schutth@wwu.edu>
2019-05-14 22:57:02 -0700

    fix device memory allocation bugs. also provide a free device mem function, which is called by gpumon_host_end

--- Project/lib/gpumon_cuda.cu
+++ Project/lib/gpumon_cuda.cu
@@ -5,40 +5,64 @@
 #include <time.h>
 #include <vector>
 
-static __device__ clock64_t* dev_tstart = nullptr;
-static __device__ clock64_t* dev_ttime = nullptr;
-static size_t dev_tbuf_size = 0;
+__device__ clock64_t* dev_tstart;
+__device__ clock64_t* dev_ttime;
+__device__ int* dev_num_iter;
 
-static __device__ int* dev_num_iter = nullptr;
+static size_t dev_tbuf_size = 0;
 static size_t dev_num_iter_size = 0;
 
-//static __device__ int* dev_number = nullptr;
+static void* d_dev_tstart = nullptr;
+static void* d_dev_ttime = nullptr;
+static void* d_dev_num_iter = nullptr;
+
+template <class T>
+void* __cuda_zalloc_sym(size_t size, const T& sym, const char* ssym) {
+	void* address_of_sym = nullptr;
+	CUDA_RUNTIME_FN(cudaGetSymbolAddress(&address_of_sym, sym));
+
+	void* device_allocated_mem = nullptr;
+	
+	CUDA_RUNTIME_FN(cudaMalloc(&device_allocated_mem, size));
+	CUDA_RUNTIME_FN(cudaMemset(device_allocated_mem, 0, size));
+	
+	CUDA_RUNTIME_FN(cudaMemcpy(address_of_sym,
+														 &device_allocated_mem,
+														 sizeof(device_allocated_mem),
+														 cudaMemcpyHostToDevice));
+
+	return device_allocated_mem;
+}
+#define cuda_zalloc_sym(sz, sym) __cuda_zalloc_sym(sz, sym, #sym)
+
+template <class T>
+void cuda_safe_free(T*& ptr) {
+	if (ptr != nullptr) {
+		CUDA_RUNTIME_FN(cudaFree(static_cast<void*>(ptr)));
+	}
+}
+
+
+EXTC HOST void gpumon_free_device_mem() {
+	cuda_safe_free(d_dev_tstart);
+	cuda_safe_free(d_dev_ttime);
+	cuda_safe_free(d_dev_num_iter);
+}
 
 EXTC HOST void gpumon_init_device_mem(int num_threads) {
-	{
-		void* dev_tstart_addr = nullptr;
-		CUDA_RUNTIME_FN(cudaGetSymbolAddress(&dev_tstart_addr, dev_tstart));
-
-		void* dev_ttime_addr = nullptr;
-		CUDA_RUNTIME_FN(cudaGetSymbolAddress(&dev_ttime_addr, dev_ttime));
-	
+	{	
 		dev_tbuf_size = sizeof(clock64_t) * static_cast<size_t>(num_threads);
-	
-		CUDA_RUNTIME_FN(cudaMalloc(&dev_tstart_addr, dev_tbuf_size));
-		CUDA_RUNTIME_FN(cudaMalloc(&dev_ttime_addr, dev_tbuf_size));
+
+		d_dev_tstart = cuda_zalloc_sym(dev_tbuf_size, dev_tstart);
+
+		d_dev_ttime = cuda_zalloc_sym(dev_tbuf_size, dev_ttime);
 	}
 
 	// test code
 	{
-		void* dev_num_iter_addr = nullptr;
-		CUDA_RUNTIME_FN(cudaGetSymbolAddress(&dev_num_iter_addr, dev_num_iter));
-
-		//	void* dev_num_size = nullptr;
-		
 		dev_num_iter_size = sizeof(int) * static_cast<size_t>(num_threads);
 
-		CUDA_RUNTIME_FN(cudaMalloc(&dev_num_iter_addr, dev_num_iter_size));
-		
+		d_dev_num_iter = cuda_zalloc_sym(dev_num_iter_size, dev_num_iter);
 		
 		std::vector<int> host_num_iter(num_threads, 0);
 
@@ -50,15 +74,18 @@ EXTC HOST void gpumon_init_device_mem(int num_threads) {
 			host_num_iter[i] = iter_min + (rand() % (iter_max - iter_min));
 		}
 
-		CUDA_RUNTIME_FN(cudaMemcpy(dev_num_iter_addr,
+		CUDA_RUNTIME_FN(cudaMemcpy(d_dev_num_iter,
 															 &host_num_iter[0],
 															 dev_num_iter_size,
 															 cudaMemcpyHostToDevice));
 	}
 }
 
-EXTC HOST void gpumon_get_device_ttime(clock64_t* out) {
-	CUDA_RUNTIME_FN(cudaMemcpyFromSymbol(out, "dev_ttime", dev_tbuf_size));
+EXTC HOST void gpumon_get_device_ttime(clock64_t* out) {	
+	CUDA_RUNTIME_FN(cudaMemcpy(out,
+														 d_dev_ttime,
+														 dev_tbuf_size,																			 
+														 cudaMemcpyDeviceToHost));
 }
 
 
