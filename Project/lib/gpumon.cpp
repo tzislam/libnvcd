60671da2905cbf8270af592dc6683522d5733141
schutth <schutth@wwu.edu>
2019-05-14 22:46:15 -0700

    default set device to 0 (for now), and block until kernel is finished

--- Project/lib/gpumon.cpp
+++ Project/lib/gpumon.cpp
@@ -1,5 +1,6 @@
 #include "gpumon.h"
 #include "gpumon.cuh"
+#include "util.h"
 
 #include <vector>
 #include <memory>
@@ -11,10 +12,14 @@ EXTC HOST void gpumon_host_start(int num_threads) {
 	host_ttime.clear();
 	host_ttime.resize(num_threads, 0);
 
+	CUDA_RUNTIME_FN(cudaSetDevice(0));
+	
 	gpumon_init_device_mem(num_threads);
 }
 
 EXTC HOST void gpumon_host_end() {
+	CUDA_RUNTIME_FN(cudaDeviceSynchronize());
+	
 	gpumon_get_device_ttime(&host_ttime[0]);
 
 	for (size_t i = 0; i < host_ttime.size(); ++i) {
