9d265aab9f2c1acbaa75ce0ebcaf3b65d9c99219
schutth <schutth@wwu.edu>
2019-05-23 12:56:22 -0700

    test script

---
+++ Project/event_test1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# all tests ran against CUDA 9.2
+
+case $1 in
+    # k80, 3.7: should fail
+    1) export BENCH_EVENTS="tex_cache_hit:branch:divergent_branch"
+        ;;
+
+    # k80, 3.7: should succeed
+    2) export BENCH_EVENTS="sm_cta_launched" 
+        ;;
+
+    # k80, 3.7: should fail, despite being defined for 2.x AND 1.x 
+    3) export BENCH_EVENTS="branch" 
+        ;;
+
+    # k80, 3.7: should succeed, but counters are all 0
+    4) export BENCH_EVENTS="l1_local_load_hit:l1_global_load_hit:l1_local_load_miss:l1_global_load_miss"
+       ;;
+
+    # k80, 3.7: should succeed, with all events reported.
+    5) export BENCH_EVENTS="l1_local_load_hit:l1_global_load_hit:l1_local_load_miss:l1_global_load_miss:ALL"
+       ;;
+    *)
+        echo "Invalid argument specified"
+        exit 1
+        ;;
+esac
+
+./run > out_$1
