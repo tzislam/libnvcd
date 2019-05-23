e21752500d077bfc07332502b3a48496533969f7
schutth <schutth@wwu.edu>
2019-05-23 01:35:06 -0700

    temp removal

--- Lab9/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mpic++ -I./perf-dump -openmp -o test matmul.c ./perf-dump/libperfdump.a /opt/hdf5/intel/mvapich2_ib/lib/libhdf5.a /opt/papi/intel/lib/libpapi.a -lz -lmpi
--- Lab9/interactive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-srun --partition=debug --pty --nodes=1 --ntasks-per-node=4 -t 00:10:00 --wait=0 --export=ALL /bin/bash
