176897fc55672987929ae45f00c1aa1811fe02f4
schutth <schutth@wwu.edu>
2019-05-24 01:30:28 -0700

    need nvcd_init(), nvcd_terminate() and nvcd_report()

--- Project/test/src/main.cu
+++ Project/test/src/main.cu
@@ -3,6 +3,8 @@
 #include "gpu.h"
 
 int main() {
+  nvcd_init();
+  
   int threads = 1024;
   
   clock64_t* thread_times = (clock64_t*) zallocNN(sizeof(thread_times[0]) * threads);
@@ -12,6 +14,10 @@ int main() {
   for (int i = 0; i < threads; ++i) {
     printf("[%i] time: %llu\n", i, thread_times[i]);
   }
+
+  nvcd_report();
+  
+  nvcd_terminate();
   
   return 0;
 }
