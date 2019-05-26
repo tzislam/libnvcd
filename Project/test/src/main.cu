b1d19a314c09d618c6ec159ecca17e09427e8369
schutth <schutth@wwu.edu>
2019-05-26 00:21:26 -0700

    offloading the actual nvcd client code to the gpu_test function, since it's more localized there.

--- Project/test/src/main.cu
+++ Project/test/src/main.cu
@@ -7,9 +7,7 @@ int main() {
   
   int threads = 1024;
   
-  clock64_t* thread_times = (clock64_t*) zallocNN(sizeof(thread_times[0]) * threads);
-
-  NVCD_EXEC_KERNEL(gpu_test_matrix_vec_mul(threads, &thread_times[0]));
+  gpu_test_matrix_vec_mul(threads);
   
   for (int i = 0; i < threads; ++i) {
     printf("[%i] time: %llu\n", i, thread_times[i]);
