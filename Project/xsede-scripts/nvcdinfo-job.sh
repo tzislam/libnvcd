249d8065946c6e372a85228e355274d6f2987ebf
schutth <schutth@wwu.edu>
2019-05-30 00:09:12 -0700

    jobscript for nvcdinfo

---
+++ Project/xsede-scripts/nvcdinfo-job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="nvcdinfo"
+#SBATCH --output="nvcdinfo.log"
+#SBATCH --partition=gpu-shared
+#SBATCH --gres=gpu:k80:1
+#SBATCH --pty
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --export=ALL
+#SBATCH -t 00:05:00
+
+source xsede-scripts/xsede_env.sh
+bin/nvcdinfo
