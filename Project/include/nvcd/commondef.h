f6035f35b98b734891a64f354782ad73b7edc804
schutth <schutth@wwu.edu>
2019-06-04 05:08:49 -0700

    helpers for printf debugging.

--- Project/include/nvcd/commondef.h
+++ Project/include/nvcd/commondef.h
@@ -16,6 +16,7 @@
 
 #if !defined(__CUDACC__) && !defined(__cplusplus)
 #include <stdbool.h>
+#include <inttypes.h>
 #endif
 
 #include <assert.h>
@@ -52,6 +53,7 @@
     }                                                 \
   } while (0)
 
+
 #define safe_free_v(p) safe_free((void**) &(p))
 
 #define V_UNSET (-1)
@@ -62,6 +64,34 @@ typedef int64_t clock64_t;
 typedef uint32_t bool32_t; // alignment and portability
 
 #define __FUNC__ __func__
+
+#define STRFMT_TAB1 "\t"
+#define STRFMT_TAB2 "\t\t"
+#define STRFMT_TAB3 "\t\t\t"
+#define STRFMT_TAB4 "\t\t\t\t"
+
+#define STRFMT_NEWL1 "\n"
+
+#define STRFMT_MEMBER_SEP ","
+
+#define STRFMT_PTR_VALUE(T, ptr) #T " " #ptr " = %p"
+#define STRFMT_STR_VALUE(T, p_str) #T " " #p_str " = %s"
+#define STRFMT_INT_VALUE(T, v, fmt) #T " " #v " = %" fmt
+
+#define STRFMT_UINT64_VALUE(v) STRFMT_INT_VALUE(uint64_t, v, PRIu64)
+#define STRFMT_UINT32_VALUE(v) STRFMT_INT_VALUE(uint32_t, v, PRIu32)
+#define STRFMT_INT32_VALUE(v) STRFMT_INT_VALUE(int32_t, v, PRId32)
+
+
+#define STRFMT_HEX32_VALUE(v) STRFMT_INT_VALUE(unsigned long, v, PRIx32)
+#define STRFMT_HEX64_VALUE(v) STRFMT_INT_VALUE(unsigned long long, v, PRIx64)
+
+#define STRFMT_BOOL_STR_VALUE(v) STRFMT_INT_VALUE(bool, v, "s")
+
+#define STRFMT_STRUCT_PTR_BEGIN(T, ptr) STRFMT_PTR_VALUE(T, ptr) ": {"
+#define STRFMT_STRUCT_PTR_END(ptr) "} END " #ptr
+
+#define STRVAL_BOOL_STR_VALUE(v) ((v) == 1 ? "true" : ((v) == 0 ? "false" : "GARBAGE"))
 
 enum {
   ENO_ERROR = 0,
