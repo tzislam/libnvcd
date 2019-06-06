ed51e2755b186c07e8122c15f4713c4497421e59
schutth <schutth@wwu.edu>
2019-06-05 19:46:55 -0700

    load module

--- Lab9/jobscript.sh
+++ Lab9/jobscript.sh
@@ -8,5 +8,7 @@
 #SBATCH -t 00:05:00
 export OMP_NUM_THREADS=24
 export PDUMP_EVENTS=PAPI_L1_DCM
+module load hdf5
+module load papi
 #export SLURM_NODEFILE=`generate_pbs_nodefile`
 ibrun --npernode 1 ./perf-dump.bin
