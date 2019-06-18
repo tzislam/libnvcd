5d81ce836dcfed876f73e0e3428ddf16d05ec2ee
schutth <schutth@wwu.edu>
2019-06-18 00:42:01 -0700

    branch efficiency test code

--- Project/test-branch/test-branch.cu
+++ Project/test-branch/test-branch.cu
@@ -10,26 +10,31 @@
 
 #include <nvcd/nvcd.cuh>
 
-#define CURAND_CALL(x) do { if((x) != CURAND_STATUS_SUCCESS) { \
-      printf("Error at %s:%d\n",__FILE__,__LINE__);            \
-return EXIT_FAILURE;}} while(0)
-
-__global__ void branch_kernel_bad(int* iterations) {
-  
+static void _curand_fn(curandStatus_t x, const char* expr, int line) {
+  if (x != CURAND_STATUS_SUCCESS) {
+    printf("Error for %s at %i. code received: %u", expr, line, x);
+    exit((int) x);
+  } 
 }
 
-int main() {
-  size_t n = 100;
-  size_t i;
+#define CURAND_CALL(x) _curand_fn(x, #x, __LINE__)
 
+template <typename T>
+T* dev_gen_random(size_t n, const T rand_min, const T rand_max) {
+  if (sizeof(T) != sizeof(unsigned int)) {
+    puts("Bad size received: must be 4 bytes in size and NOT floating point\n");
+    exit(0);
+  }
+  
   curandGenerator_t gen;
-  unsigned int *devData, *hostData;
+  
+  T *devData, *hostData;
   
   /* Allocate n ints on host */
-  hostData = (unsigned int *)calloc(n, sizeof(unsigned int));
+  hostData = (T *)calloc(n, sizeof(T));
 
   /* Allocate n ints on device */
-  CUDA_RUNTIME_FN(cudaMalloc((void **)&devData, n * sizeof(unsigned int)));
+  CUDA_RUNTIME_FN(cudaMalloc((void **)&devData, n * sizeof(T)));
 
   /* Create pseudo-random number generator */
   CURAND_CALL(curandCreateGenerator(&gen,
@@ -44,13 +49,13 @@ int main() {
   /* Copy device memory to host */
   CUDA_RUNTIME_FN(cudaMemcpy(hostData,
                        devData,
-                       n * sizeof(unsigned int),
+                       n * sizeof(T),
                        cudaMemcpyDeviceToHost));
 
   /* Set seed */
-  CURAND_CALL(curandSetPseudoRandomGeneratorSeed(gen, 1234ULL));
+  CURAND_CALL(curandSetPseudoRandomGeneratorSeed(gen, (unsigned long long) time(NULL)));
 
-  /* Generate n floats on device */
+  /* Generate n Ts on device */
   CURAND_CALL(curandGenerate(gen,
                              devData,
                              n));
@@ -58,21 +63,68 @@ int main() {
   /* Copy device memory to host */
   CUDA_RUNTIME_FN(cudaMemcpy(hostData,
                        devData,
-                       n * sizeof(unsigned int),
+                       n * sizeof(T),
                        cudaMemcpyDeviceToHost));
   
-  /* Show result */
+  size_t i;
   for(i = 0; i < n; i++) {
-    printf("Value [%" PRIu64  "]: %" PRIu32 "\n", i, hostData[i]);
+    hostData[i] = rand_min + hostData[i] % (rand_max - rand_min);
   }
 
-  printf("%s", "\n");
+  CUDA_RUNTIME_FN(cudaMemcpy(devData,
+                             hostData,
+                             n * sizeof(T),
+                             cudaMemcpyHostToDevice));
 
   /* Cleanup */
   CURAND_CALL(curandDestroyGenerator(gen));
-  CUDA_RUNTIME_FN(cudaFree(devData));
 
   free(hostData);
+
+  return devData;
+}
+
+
+__global__ void branch_kernel0(unsigned int* iterations) {
+  int thread = blockDim.x * blockIdx.x + threadIdx.x;
+  
+  printf("\n\ngridDim.x: %i, blockIdx.x: %i, blockDim.x: %i, threadIdx.x: %i, thread: %i\n"
+         "Kernel value [%" PRId32  "]: %" PRIu64 " \n",
+         gridDim.x,
+         blockIdx.x,
+         blockDim.x,
+         threadIdx.x,
+         thread,
+         thread,
+         (size_t) iterations[thread]);
+
+  volatile unsigned int num_iterations = iterations[thread];
+
+  if ((num_iterations & 0x1) == 0) {
+    num_iterations = num_iterations << 1;   
+  }
+
+  volatile unsigned int i = 0;
+  
+  while (i < num_iterations) {
+    i++;
+  }
+}
+
+int main() {
+  size_t n = 30;
+  unsigned int a = 5;
+  unsigned int b = 50;
+  unsigned int* device_iterations = dev_gen_random(n, a, b);
+
+  size_t nblk = 2;
+  
+  dim3 Dg(nblk, 1, 1);
+  dim3 Db(n / nblk, 1, 1);
+  
+  branch_kernel0<<<Dg, Db>>>(device_iterations);
+  
+  CUDA_RUNTIME_FN(cudaFree(device_iterations));
 
   return EXIT_SUCCESS;
 }
