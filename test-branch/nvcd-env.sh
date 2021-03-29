3b7e3d118e03e573d2973da55d28efbb2233fccb
Holland Schutte <hgschutte1@gmail.com>
2021-03-28 22:12:35 -0700

    [cleanup1] old test code that's no longer necessary

--- test-branch/nvcd-env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NVCD_HOME/bin
-export BENCH_EVENTS="branch:divergent_branch:threads_launched:warps_launched"
-export BENCH_METRICS="branch_efficiency:stall_constant_memory_dependency:stall_exec_dependency:stall_inst_fetch:stall_memory_dependency:stall_memory_throttle:warp_execution_efficiency:warp_nonpred_execution_efficiency"
